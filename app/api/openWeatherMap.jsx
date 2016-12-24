e4d72a32052a40ae86ce5b5749184da5202cefea
AMIT PRAKASH PANDEY <AMIT PRAKASH PANDEY>
2016-12-24 06:51:05 +0530

    initial commit.

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,23 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=ebf475a72cc2f2d52e9a5b6f3e788140&units=imperial';
+
+//ebf475a72cc2f2d52e9a5b6f3e788140
+module.exports = {
+  getTemp :function(location){
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+    console.log(requestUrl , 'requestUrl')
+    return axios.get(requestUrl).then(function(resp){
+      console.log(resp , 'resp');
+      if(resp.data.cod && resp.data.message){
+        throw new Error(resp.data.message);
+      }else{
+        return resp.data.main.temp;
+      }
+    },function(err){
+      console.log(err , 'err.message');
+      throw new Error(err.message);
+    })
+  }
+}
---
+++ app/components/Weather.jsx
@@ -0,0 +1,46 @@
+var React = require('react');
+var WeatherForm = require('WeatherForm');
+var WeatherMessage = require('WeatherMessage');
+var Openweathermap = require('Openweathermap');
+var Weather = React.createClass({
+  getInitialState:function(){
+    return{
+      isLoading:false
+    }
+  },
+  handleSearch:function(location){
+    debugger;
+    var that = this;
+    this.setState({isLoading:true})
+    Openweathermap.getTemp(location).then(function(temp){
+      that.setState({
+        location:location,
+        temp:temp,
+        isLoading:false
+      });
+    },function(errorMessage){
+      that.setState({
+        isLoading:false
+      });
+      alert(errorMessage);
+    })
+  },
+  render: function () {
+    var {isLoading, temp,location} = this.state;
+    function renderMessage(){
+      if(isLoading){
+        return <h3>Fetching Weather...</h3>
+      }else if (temp && location) {
+        return <WeatherMessage temp={temp} location={location}/>;
+      }
+    }
+    return (
+      <div>
+        <WeatherForm onSearch={this.handleSearch}/>
+        {renderMessage()}
+      </div>
+    );
+  }
+});
+
+module.exports = Weather;
